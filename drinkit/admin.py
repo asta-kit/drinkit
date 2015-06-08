c545e7263d99112731a438bb2248668e155ea875
Michael Tänzer <neo@nhng.de>
2015-06-09 01:00:48 +0200

    Add admin interface and update Transaction.amount when changing consumption

--- drinkit/admin.py
+++ drinkit/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from django.db import models
 
-# Register your models here.
+from .models import Drink, Drinker, Transaction, Consumption
+
+@admin.register(Drink)
+class DrinkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'active')
+
+
+class TransactionInline(admin.TabularInline):
+    model = Transaction
+    extra = 0
+
+class BalanceFilter(admin.SimpleListFilter):
+    title = 'Balance less than'
+    parameter_name = 'balance'
+    def lookups(self, request, model_admin):
+        return ((-500,-500), (-100,-100), (-50,-50), (0,0))
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.annotate(_balance=models.Sum('transaction__amount')).filter(_balance__lt=self.value())
+
+@admin.register(Drinker)
+class DrinkerAdmin(admin.ModelAdmin):
+    inlines = (TransactionInline,)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.annotate(_balance=models.Sum('transaction__amount'))
+        return qs
+
+    def balance(self, drinker):
+        return drinker._balance
+    balance.admin_order_field = '_balance'
+
+    list_display = ('firstname', 'lastname', 'balance', 'active')
+    list_filter = ('active', BalanceFilter)
+    search_fields = ('firstname', 'lastname', 'email')
+
+
+class ConsumptionInline(admin.TabularInline):
+    model = Consumption
+    extra = 0
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    inlines = (ConsumptionInline,)
+    list_display = ('drinker', 'date', 'amount')
+    date_hierarchy = 'date'
--- drinkit/models.py
+++ drinkit/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from decimal import Decimal
 
 class Drink(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ('price',)
 
 class Drinker(models.Model):
     firstname = models.CharField(max_length=20)
@@ -25,13 +29,38 @@ class Drinker(models.Model):
     def __str__(self):
         return self.fullname
 
+    class Meta:
+        ordering = ('firstname', 'lastname')
+
 class Transaction(models.Model):
     drinker = models.ForeignKey(Drinker)
     date = models.DateField()
     amount = models.DecimalField(max_digits=6, decimal_places=2)
     reckoning = models.ManyToManyField(Drink, through='Consumption')
 
+    def __str__(self):
+        return '{} -> {}: {} €'.format(self.date, self.drinker, self.amount)
+
+    def save(self, *args, **kwargs):
+        consumption = self.consumption_set.all()
+        if len(consumption):
+            self.amount = Decimal(0)
+            for c in consumption:
+                self.amount -= c.count * c.drink.price
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ('-date', 'drinker')
+
 class Consumption(models.Model):
     drink = models.ForeignKey(Drink)
     transaction = models.ForeignKey(Transaction)
     count = models.PositiveSmallIntegerField()
+
+    def save(self, *args, **kwargs):
+        res = super().save(*args, **kwargs)
+        self.transaction.save()
+        return res
+
+    class Meta:
+        unique_together = ('drink', 'transaction')
